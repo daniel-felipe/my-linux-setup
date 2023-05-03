59181ba8ff89da860556655fd0819608704f16f5
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-05-02 22:07:54 -0300

    feat: adding docker installation

--- setup/ubuntu_setup.sh
+++ setup/ubuntu_setup.sh
@@ -30,6 +30,24 @@ wget -qO - https://deb.tableplus.com/apt.tableplus.com.gpg.key | gpg --dearmor |
 sudo add-apt-repository "deb [arch=amd64] https://deb.tableplus.com/debian/22 tableplus main"
 sudo apt update -y && sudo apt install tableplus -y
 
+# -- [ INSTALLING DOCKER ]
+echo "[+] Istalling Docker"
+sudo apt-get update -y && apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
 # --- [ INSTALLING OH MY ZSH ]
 echo "[+] Istalling Oh My Zsh"
 chsh -s $(which zsh)
